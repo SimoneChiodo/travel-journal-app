b365bc766138cf37d67c6dfbae7574cf41d0e730
SimoneChiodo <simochi.005@gmail.com>
2025-08-20 18:21:42 +0200

    Fix centered map in Dashboard page

--- src/main/resources/static/js/maps.js
+++ src/main/resources/static/js/maps.js
@@ -2,7 +2,7 @@
 const travels = document.querySelectorAll('.travels');
 
 // Initialize the map
-const map = L.map('map').setView([41.9, 12.5], 6); // Default view on Rome
+const map = L.map('map').setView([0, 0], 2); // Default view on Rome
 
 // Add OpenStreetMap tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
